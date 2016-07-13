0dc8e3eea811f2056bcd375c7c0310bbca6507e1
Power House <Power House@DESKTOP-GTPF2EM>
2016-07-14 00:16:35 +0530

    Complete Code

--- Example_1/src/com/mcapatna/manager/AppMgr.java
+++ Example_1/src/com/mcapatna/manager/AppMgr.java
@@ -4,11 +4,36 @@ import com.mcapatna.pojo.A;
 import com.mcapatna.pojo.B;
 
 public class AppMgr {
+	public AppMgr() {
+		super();
+	}
+
 	A aobj;
 	B bobj;
-	public void show(){
+
+	public void show() {
 		aobj.showA();
 		bobj.show();
+	}
+
+	public static void main(String[] args) {
+		System.out.println("AppMgr.main()");
+	}
+
+	public A getAobj() {
+		return aobj;
+	}
+
+	public void setAobj(A aobj) {
+		this.aobj = aobj;
+	}
+
+	public B getBobj() {
+		return bobj;
+	}
+
+	public void setBobj(B bobj) {
+		this.bobj = bobj;
 	}
 
 }
--- Example_1/src/com/mcapatna/pojo/A.java
+++ Example_1/src/com/mcapatna/pojo/A.java
@@ -19,5 +19,4 @@ public class A {
 		System.out.println(a);
 		System.out.println(str);
 	}
-
 }
